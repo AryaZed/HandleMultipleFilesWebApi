c6b0a35a9c4594a742fbe47abefaeadfa7ba549f
Arya Zarpak <aria@smartx.ir>
2023-12-27 15:45:42 +0330

    all commit 1

--- HandleMultipleFilesWebApi/Service/Process/ProcessService.cs
+++ HandleMultipleFilesWebApi/Service/Process/ProcessService.cs
@@ -24,75 +24,49 @@ namespace HandleMultipleFilesWebApi.Service.Process
             _hubContext = hubContext;
         }
 
-        //public async Task ProcessFilesAsync(List<string> fileNames, string jobId)
-        //{
-        //    var datetime = DateTime.Now;
-        //    var zipFileName = $"output-{datetime:HH-mm-ss}.zip";
-        //    var bucketName = string.Empty;
-
-        //    try
-        //    {
-        //        SaveJob(jobId);
-        //        using var zipMemoryStream = new MemoryStream();
-        //        using (var zip = new ZipArchive(zipMemoryStream, ZipArchiveMode.Create, true))
-        //        {
-        //            foreach (var item in fileNames)
-        //            {
-        //                var urlSplit = item.Split("/");
-        //                if (urlSplit.Length < 3)
-        //                {
-        //                    _logger.LogWarning($"Invalid file path: {item}");
-        //                    continue;
-        //                }
-
-        //                bucketName = urlSplit[0];
-        //                var objectKey = string.Join("/", urlSplit.Skip(1));
-        //                var zipEntry = zip.CreateEntry(Path.GetFileName(objectKey));
-
-        //                using var entryStream = zipEntry.Open();
-        //                var getObjectArgs = new GetObjectArgs()
-        //                    .WithBucket(bucketName)
-        //                    .WithObject(objectKey)
-        //                    .WithCallbackStream(stream => stream.CopyTo(entryStream));
-        //                await _minioService.GetObjectAsync(getObjectArgs);
-        //            }
-        //        }
-
-        //        if (zipMemoryStream.Length == 0)
-        //        {
-        //            _logger.LogWarning("No files found to zip for job " + jobId);
-        //            await _hubContext.Clients.All.SendAsync("ReceiveJobStatus", jobId, new { Status = "NoFilesFound" });
-        //            return; // Exit if no files were added to the zip
-        //        }
-
-        //        await UploadZipToMinio(zipMemoryStream, bucketName, zipFileName);
-        //        var presignedUrl = await GeneratePresignedUrl(bucketName, zipFileName);
-
-
-
-        //        await _hubContext.Clients.All.SendAsync("ReceiveJobStatus", jobId, new { Status = "Completed", Url = presignedUrl });
-        //        //// Save or send the presigned URL along with the job ID for later retrieval
-        //        //// This depends on how you track job statuses and results
-        //        SaveJobResult(jobId, presignedUrl);
-        //        _logger.LogInformation($"Sent 'Completed' status with URL for job {jobId}");
-
-        //        //var test = _memoryCache.TryGetValue<JobResult>(jobId, out var jobs);
-        //    }
-        //    catch (Exception ex)
-        //    {
-        //        _logger.LogError(ex, "Error in ProcessFilesAsync for job " + jobId);
-        //        // Handle error, update job status accordingly
-        //    }
-        //}
-
         public async Task ProcessFilesAsync(List<string> fileNames, string jobId)
         {
             var datetime = DateTime.Now;
-            var presignedUrl = "https://www.google.com";
+            var zipFileName = $"output-{datetime:HH-mm-ss}.zip";
+            var bucketName = string.Empty;
+
             try
             {
+                SaveJob(jobId);
+                using var zipMemoryStream = new MemoryStream();
+                using (var zip = new ZipArchive(zipMemoryStream, ZipArchiveMode.Create, true))
+                {
+                    foreach (var item in fileNames)
+                    {
+                        var urlSplit = item.Split("/");
+                        if (urlSplit.Length < 3)
+                        {
+                            _logger.LogWarning($"Invalid file path: {item}");
+                            continue;
+                        }
 
-                await Task.Delay(5000);
+                        bucketName = urlSplit[0];
+                        var objectKey = string.Join("/", urlSplit.Skip(1));
+                        var zipEntry = zip.CreateEntry(Path.GetFileName(objectKey));
+
+                        using var entryStream = zipEntry.Open();
+                        var getObjectArgs = new GetObjectArgs()
+                            .WithBucket(bucketName)
+                            .WithObject(objectKey)
+                            .WithCallbackStream(stream => stream.CopyTo(entryStream));
+                        await _minioService.GetObjectAsync(getObjectArgs);
+                    }
+                }
+
+                if (zipMemoryStream.Length == 0)
+                {
+                    _logger.LogWarning("No files found to zip for job " + jobId);
+                    await _hubContext.Clients.All.SendAsync("ReceiveJobStatus", jobId, new { Status = "NoFilesFound" });
+                    return; // Exit if no files were added to the zip
+                }
+
+                await UploadZipToMinio(zipMemoryStream, bucketName, zipFileName);
+                var presignedUrl = await GeneratePresignedUrl(bucketName, zipFileName);
 
                 await _hubContext.Clients.All.SendAsync("ReceiveJobStatus", jobId, new { Status = "Completed", Url = presignedUrl });
                 //// Save or send the presigned URL along with the job ID for later retrieval
@@ -108,6 +82,30 @@ namespace HandleMultipleFilesWebApi.Service.Process
                 // Handle error, update job status accordingly
             }
         }
+
+        //public async Task ProcessFilesAsync(List<string> fileNames, string jobId)
+        //{
+        //    var datetime = DateTime.Now;
+        //    var presignedUrl = "https://www.google.com";
+        //    try
+        //    {
+
+        //        await Task.Delay(5000);
+
+        //        await _hubContext.Clients.All.SendAsync("ReceiveJobStatus", jobId, new { Status = "Completed", Url = presignedUrl });
+        //        //// Save or send the presigned URL along with the job ID for later retrieval
+        //        //// This depends on how you track job statuses and results
+        //        SaveJobResult(jobId, presignedUrl);
+        //        _logger.LogInformation($"Sent 'Completed' status with URL for job {jobId}");
+
+        //        //var test = _memoryCache.TryGetValue<JobResult>(jobId, out var jobs);
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        _logger.LogError(ex, "Error in ProcessFilesAsync for job " + jobId);
+        //        // Handle error, update job status accordingly
+        //    }
+        //}
 
         public void SaveJobResult(string jobId, string presignedUrl)
         {
